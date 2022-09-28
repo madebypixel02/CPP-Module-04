9971e510fb677fc56d708d67ecc03bd2e9d65f91
madebypixel02 <madebypixel02@proton.me>
2022-09-28 17:02:54 +0200

    Added ex03
    Details:
    - Added materia to gitignore
    - Added first working version of ex03 (at last)

---
+++ ex03/Character.hpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Character.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aperez-b <aperez-b@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/28 11:35:44 by aperez-b          #+#    #+#             */
+/*   Updated: 2022/09/28 14:28:09 by aperez-b         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include "ICharacter.hpp"
+
+class Character: public ICharacter
+{
+	private:
+		std::string	_name;
+		AMateria	*_inventory[4];
+	public:
+		/* Constructors & Destructors */
+		Character(void);
+		Character(std::string const &name);
+		Character(Character const &copy);
+		~Character(void);
+
+		/* Basic Operators */
+		Character const	&operator=(Character const &copy);
+
+		/* Getters & Setters */
+		std::string const	&getName(void) const;
+		void				setName(std::string const &name);
+
+		/* Main Member Functions */
+		virtual void	equip(AMateria *m);
+		virtual void	unequip(int idx);
+		virtual void	use(int idx, ICharacter &target);
+};
